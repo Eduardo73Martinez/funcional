54168825931a32616a00602ff5616f10ab155853
Eduardo73Martinez <martin61martinez@hotmail.com.ar>
2024-06-05 18:45:17 -0300

    Practica10 casi terminada falta algunos ejercios

--- Practica10/Rowstones.hs
+++ Practica10/Rowstones.hs
@@ -47,43 +47,90 @@ evalExpRBool              (Lit x) t = x
 evalExpRBool       (PuedeMover d) t = puedeMover d t 
 evalExpRBool         (NroBolitas) t = boom "operación inválida" t 
 evalExpRBool         (HayBolitas) t = hayBolitas t 
-evalExpRBool       (UnOp uOp exp) t = evalExpRBool exp
+evalExpRBool       (UnOp uOp exp) t = evalExpRBool exp t 
 evalExpRBool (BinOp op exp1 exp2) t = evalOpBinBool op (evalExpRBool exp1 t)  (evalExpRBool exp2 t) t
 
 evalOpBinBool :: BOp -> Bool-> Bool -> TableroR-> Bool 
 evalOpBinBool YTambien x y t  = x && y
 evalOpBinBool OBien    x y t= x || y
 evalOpBinBool _ _ _ t = boom "operación inválida" t 
+
 -- iii. 
 expRTieneTipoInt :: ExpR Int -> Bool
 -- , que indica si la
 -- expresión dada no falla cuando se ejecuta evalExpRInt.
-expRTieneTipoInt = undefined
+expRTieneTipoInt              (Lit x) = True
+expRTieneTipoInt       (PuedeMover d) = False
+expRTieneTipoInt         (NroBolitas) = True  
+expRTieneTipoInt         (HayBolitas) = False 
+expRTieneTipoInt       (UnOp uOp exp) = False
+expRTieneTipoInt (BinOp op exp1 exp2) =  isTypeBinOPInt op (exp1)  (exp2) 
+                                    
+
+isTypeBinOPInt :: BOp -> ExpR Int-> ExpR Int -> Bool 
+isTypeBinOPInt Mas x y = expRTieneTipoInt x  && expRTieneTipoInt y
+isTypeBinOPInt Por x y = expRTieneTipoInt x  && expRTieneTipoInt y
+isTypeBinOPInt _   _ _ = False 
 
 -- iv. 
 expRTieneTipoBool :: ExpR Bool -> Bool
 -- , que indica si la
 -- expresión dada no falla cuando se ejecuta evalExpRBool.
-expRTieneTipoBool = undefined
+expRTieneTipoBool              (Lit x) = True
+expRTieneTipoBool       (PuedeMover d) = False
+expRTieneTipoBool         (NroBolitas) = True  
+expRTieneTipoBool         (HayBolitas) = False 
+expRTieneTipoBool       (UnOp uOp exp) = False
+expRTieneTipoBool (BinOp op exp1 exp2) =  isTypeBinOPBool op (exp1)  (exp2) 
+                                    
+
+isTypeBinOPBool :: BOp -> ExpR Bool-> ExpR Bool -> Bool 
+isTypeBinOPBool OBien    x y = expRTieneTipoBool x  && expRTieneTipoBool y
+isTypeBinOPBool YTambien x y = expRTieneTipoBool x  && expRTieneTipoBool y
+isTypeBinOPBool        _ _ _ = False 
 
 -- v. 
 evalR :: ComandoR -> TableroR -> TableroR
 -- , que describe
 -- el tablero resultante de evaluar el comando dado en el tablero dado.
-evalR = undefined
+evalR         (Mover d) t = mover d t 
+evalR           (Poner) t = poner t 
+evalR           (Sacar) t = sacar t 
+evalR            (NoOp) t = boom "no se puede realizar ninguna operacion" t
+evalR  (Repetir (Lit n) c) t = if  n >= 0 
+                                then evalR (Repetir (Lit (n-1)) c ) t
+                                else t 
+                                
+evalR (Mientras expB c) t = if  bool
+                                then evalR (Mientras expB  c ) t
+                                else t  
+                                where bool = evalExpRBool expB t  --- ¿no hay un loop infinito aca?
+evalR (Secuencia c1 c2) t = evalR c1 (evalR c2 t)          
 
 -- vi. 
 cantSacar :: ComandoR -> Int
 -- , que describe la cantidad de
 -- constructores Sacar que hay en el comando dado.
-cantSacar = undefined
+cantSacar         (Mover d) = 0
+cantSacar           (Poner) = 0
+cantSacar           (Sacar) = 1 
+cantSacar            (NoOp) = 0 
+cantSacar(Repetir (expN) c) = cantSacar c                
+cantSacar (Mientras expB c) = cantSacar c    
+cantSacar (Secuencia c1 c2) = cantSacar c1 +  cantSacar c2      
 
 -- vii. 
 seqN :: Int -> ComandoR -> ComandoR
 -- , que describe la
 -- secuencia de comandos que reitera el comando dado la cantidad de
 -- veces dada.
-seqN = undefined
+seqN          (Mover d) = 0
+seqN            (Poner) = 0
+seqN            (Sacar) = 1 
+seqN             (NoOp) = 0 
+seqN (Repetir (expN) c) = cantSacar c                
+seqN  (Mientras expB c) = cantSacar c    
+seqN  (Secuencia c1 c2) = cantSacar c1 +  cantSacar c2      
 
 -- viii. 
 repeat2Seq :: ComandoR -> ComandoR
